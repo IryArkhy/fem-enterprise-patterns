898aa9ecdaeb315bb187da6e7d8b876acf0842f6
Iryna Arkhypchenko <37odri@gmail.com>
2021-07-01 22:51:29 +0300

    first exercise - refactoring code

--- apps/micro/src/app/home/home.component.ts
+++ apps/micro/src/app/home/home.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { Widget } from '@fem/api-interfaces';
 import { v4 as uuidv4 } from 'uuid';
 
+enum IMode {
+  create,
+  update,
+  delete,
+}
+
 @Component({
   selector: 'fem-home',
   templateUrl: './home.component.html',
@@ -9,26 +15,46 @@ import { v4 as uuidv4 } from 'uuid';
 })
 export class HomeComponent {
   price;
-  mode;
+  mode: IMode;
   widgets: Widget[];
 
-  updateWidgetsAndRecalculateCost(widget: Widget) {
-    switch (this.mode) {
-      case 'create':
-        const newWidget = Object.assign({}, widget, { id: uuidv4() });
-        this.widgets = [...this.widgets, newWidget];
-        break;
-      case 'update':
-        this.widgets = this.widgets.map(_widget => widget.id === _widget.id
-          ? Object.assign({}, widget) : _widget);
-        break;
-      case 'delete':
-        this.widgets = this.widgets.filter(_widget => widget.id !== _widget.id);
-        break;
+  updateWidgetsAndRecalculateCost(widget: Widget, widgets: Widget[], mode: IMode,) {
+    this.widgets = this.updateWidgets(widget, widgets, mode);
+
+    this.price = this.getTotalPrice(widgets);
+  }
+
+  updateWidgets(widget: Widget, widgets: Widget[], mode: IMode,) {
+    switch (mode) {
+      case IMode.create:
+        return this.addWidget(widget, widgets);
+
+      case IMode.update:
+        return this.updateWidget(widget, widgets);
+
+      case IMode.delete:
+        return this.deleteWidget(widget, widgets);
+
       default:
         break;
     }
-    return this.widgets.reduce((acc, curr) => acc + curr.price, 0);
+  }
+
+  addWidget(widget: Widget, widgets: Widget[]) {
+    const newWidget = Object.assign({}, widget, { id: uuidv4() });
+    return  [...widgets, newWidget];
+  }
+
+  updateWidget(widget: Widget, widgets: Widget[]) {
+    return widgets.map(_widget => widget.id === _widget.id ? Object.assign({}, widget) : _widget);
+  }
+
+  deleteWidget(widget: Widget, widgets: Widget[]) {
+    return widgets.filter(_widget => widget.id !== _widget.id);
+  }
+
+  getTotalPrice(widgets: Widget[]) {
+    return widgets.reduce((acc, curr) => acc + curr.price, 0)
   }
 
 }
